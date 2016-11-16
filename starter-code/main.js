6db35770086a79cd108baec723b3147b57628cff
CLICKON_PETS EDITOR <sharonwong1@Sharons-MacBook-Pro.local>
2016-11-16 16:32:27 +0000

    Pushing latest version of memorygame

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,3 +8,28 @@ if (cardOne===cardTwo) {
 } else {
 	alert("Sorry, try again")
 }
+
+var gameBoard=document.getElementsByClassName('board').id='game-board';
+var board=document.getElementById('game-board');
+function createBoard () {
+for (var i = 0; i<cards.length; i++) {
+ var cardElement = document.createElement('div');
+ cardElement.className = 'card';
+ cardElement.setAttribute('data-card', cards [i]);
+ cardElement.addEventListener('click', isTwoCards);
+ cardElement.innerHTML()
+board.appendChild(cardElement);
+board.appendChild(cardElement);
+};
+
+function isTwoCards () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card' === 'king') {
+		this.innerHTML = "<img src='https://pixabay.com/en/crown-jewel-king-ornate-queen-1296751/'>";
+	} else {
+		this.innerHTML= "<img src='https://pixabay.com/en/crown-king-queen-royal-royalty-1515871/'>";
+	} 
+
+
+
